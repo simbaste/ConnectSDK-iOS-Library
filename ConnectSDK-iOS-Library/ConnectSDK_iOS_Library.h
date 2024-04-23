063ba7e5a0f83ce7ace48df3d901482270084b38
Stephane MBA <stephane.mba@netgem.com>
2024-04-23 11:56:12 +0200

    Initial Commit

---
+++ ConnectSDK-iOS-Library/ConnectSDK_iOS_Library.h
@@ -0,0 +1,18 @@
+//
+//  ConnectSDK_iOS_Library.h
+//  ConnectSDK-iOS-Library
+//
+//  Created by Stephane SIMO MBA on 23/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConnectSDK_iOS_Library.
+FOUNDATION_EXPORT double ConnectSDK_iOS_LibraryVersionNumber;
+
+//! Project version string for ConnectSDK_iOS_Library.
+FOUNDATION_EXPORT const unsigned char ConnectSDK_iOS_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectSDK_iOS_Library/PublicHeader.h>
+
+
